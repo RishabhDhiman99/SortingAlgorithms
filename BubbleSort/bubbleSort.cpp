2c1dce5e37cd5cfe37c4abed76e99cdac1f11ecb
Rishabh Dhiman <51153454+RishabhDhiman99@users.noreply.github.com>
2020-01-23 21:48:28 +0530

    Add files via upload

---
+++ BubbleSort/bubbleSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+	//declaring variables and array
+	int n,i,j,temp;
+	int arr[100];
+	//taking user input for number of terms in array
+	cout<<"Enter number of terms: "<<endl;
+	cin>>n;
+	//taking array as input from user
+	cout<<"Enter array: "<<endl;
+	for(i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	//sorting algorithm
+	for(i=0;i<n-1;i++){
+		for(j=i;j<n-i-1;j++){
+			//swap
+			if(arr[j]>arr[j+1]){
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	//printing out sorted array
+	cout<<"Sorted array: "<<endl;
+	for(i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
